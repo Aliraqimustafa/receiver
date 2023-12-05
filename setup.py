ef7a09eaae3fb3493b4b6a75aa4a536ad6007ac1
Mustafa Mohammad <126193386+Aliraqimustafa@users.noreply.github.com>
2023-12-05 09:10:26 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='receiver',
+    version='0.1.0',
+    py_modules=['receiver.receiver'],
+    install_requires=[
+        # 'pyngrok',
+        # 'Flask',
+    ],
+)
